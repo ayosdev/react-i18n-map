01937a482bd6e8639e6c002726fff8bd1d9b3a13
chris <deezahyn@gmail.com>
2016-12-05 14:04:17 +0800

    add test for utils

---
+++ src/utils.test.js
@@ -0,0 +1,29 @@
+import { template } from './utils'
+
+describe('template', () => {
+
+	test('is function', () => {
+		expect(typeof template == 'function').toBe(true)
+	})
+
+	test('returns blank if message is blank', () => {
+		const actual = template('')
+		expect(actual).toBe('')
+	})
+
+	test('returns message', () => {
+		const actual = template('hello')
+		expect(actual).toBe('hello')
+	})
+
+	test('returns interpolated message', () => {
+		const actual = template('hello {foo}', { foo: 'world'})
+		expect(actual).toBe('hello world')
+	})
+
+	test('does not throw if value is not an object', () => {
+		const actual = template('hello world', 'test')
+		expect(actual).toBe('hello world')
+	})
+
+})
